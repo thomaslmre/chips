3f55a8be7cc84b6d65bddefd875915fa62d599c0
thomaslmre <133402638+thomaslmre@users.noreply.github.com>
2023-05-13 20:56:18 +0200

    Update database.js

--- database.js
+++ database.js
@@ -7,17 +7,18 @@ app.use(cors());
 
 // Créer une connexion à la base de données
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "oneway"
+  host: "89.116.147.103",
+  user: "u104222462_ONW_thomas",
+  password: "L&@r7IUFX0p",
+  database: "u104222462_oneway",
+  port: 3306
 });
 
 // Route GET pour récupérer des données depuis la base de données
 app.get('/donnees', (req, res) => {
-  const sql = `SELECT Femme.nom_femme, Femme.prenom_femme, Femme.annee_naissance_femme, atout1.rang_atout as tete_femme, atout2.rang_atout as back_femme FROM Femme 
-               LEFT JOIN Atout atout1 ON Femme.tete_femme = atout1.id_atout
-               LEFT JOIN Atout atout2 ON Femme.back_femme = atout2.id_atout`;
+  const sql = `SELECT femme.nom_femme, femme.prenom_femme, femme.annee_naissance_femme, atout1.rang_atout as tete_femme, atout2.rang_atout as back_femme FROM femme 
+               LEFT JOIN atout atout1 ON femme.tete_femme = atout1.id_atout
+               LEFT JOIN atout atout2 ON femme.back_femme = atout2.id_atout`;
   connection.query(sql, (err, results) => {
     if (err) {
       console.error('Erreur lors de l\'exécution de la requête :', err);
